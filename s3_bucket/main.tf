10c24dd9abeef3649a0be22f105cdea9501e5be3
Iván Sainz <ivan.sainz@aheadlabs.com>
2021-04-19 12:21:24 +0200

    Added output variable for the bucket name.

--- s3_bucket/main.tf
+++ s3_bucket/main.tf
@@ -62,3 +62,7 @@ resource "aws_s3_bucket_public_access_block" "s3_bucket_public_access_block" {
     ignore_public_acls = var.block_public_access_any_acls
     restrict_public_buckets = var.block_public_access_cross_accounts
 }
+
+output "bucket" {
+    value = var.bucket_name
+}
