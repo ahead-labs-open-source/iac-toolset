0e9989fceb19cab1e437b689e3b2bd0371f5c85d
Iván Sainz <ivan.sainz@aheadlabs.com>
2021-04-19 02:46:37 +0200

    Added s3_bucket module.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Terraform modules #
+
+This repository contains Terraform modules to be used in any Terraform project in order to simplify things and reuse code.
+
+## More information ##
+
+* [Terraform module sources](https://www.terraform.io/docs/language/modules/sources.html)
+* [Hashicorp language specification](https://github.com/hashicorp/hcl/blob/main/hclsyntax/spec.md)
---
+++ s3_bucket/main.tf
@@ -0,0 +1,64 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0"
+      configuration_aliases = [ aws.default, aws.global ]
+    }
+  }
+}
+
+variable "bucket_name" {
+    type = string
+}
+variable "bucket_acl" {
+    type = string
+    default = "private"
+}
+variable "bucket_tags" {
+    type = map
+    default = {}
+}
+variable "block_public_access_new_acls" {
+    type = bool
+    default = true
+}
+variable "block_public_access_any_acls" {
+    type = bool
+    default = true
+}
+variable "block_public_access_policies" {
+    type = bool
+    default = true
+}
+variable "block_public_access_cross_accounts" {
+    type = bool
+    default = true
+}
+variable "bucket_versioning" {
+    type = bool
+    default = false
+}
+
+resource "aws_s3_bucket" "s3_bucket" {
+    provider = aws.default
+
+    bucket = var.bucket_name
+    acl = var.bucket_acl
+    tags = var.bucket_tags
+
+    versioning {
+        enabled = var.bucket_versioning
+    }
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_bucket_public_access_block" {
+    provider = aws.default
+
+    bucket = aws_s3_bucket.s3_bucket.id
+
+    block_public_acls = var.block_public_access_new_acls
+    block_public_policy = var.block_public_access_policies
+    ignore_public_acls = var.block_public_access_any_acls
+    restrict_public_buckets = var.block_public_access_cross_accounts
+}
