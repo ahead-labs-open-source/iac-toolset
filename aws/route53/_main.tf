7372b603b6bedabbad6da0ca5563df1b59516eb2
Iván Sainz <ivan.sainz@aheadlabs.com>
2021-04-30 02:10:28 +0200

    Changed file structure for the route53 module.

--- aws/route53/_main.tf
+++ aws/route53/_main.tf
@@ -9,9 +9,3 @@ terraform {
     }
   }
 }
-
-variable "domain" {type = string}
-variable "zone_id" {type = string}
-
-variable "office_365_email" {type = bool}
-variable "office_365_email_msid" {type = string}
