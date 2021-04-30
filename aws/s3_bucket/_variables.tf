d19693bc5a8cb5cb89443d747edfdb75842410e2
Iván Sainz <ivan.sainz@aheadlabs.com>
2021-04-30 02:04:18 +0200

    Added web capabilities to S3 buckets.

---
+++ aws/s3_bucket/_variables.tf
@@ -0,0 +1,56 @@
+variable "bucket_name" {
+    type = string
+}
+variable "bucket_acl" {
+    type = string
+    default = "private"
+}
+variable "bucket_tags" {
+    type = map
+    default = {}
+}
+variable "block_public_access_new_acls" {
+    type = bool
+    default = true
+}
+variable "block_public_access_any_acls" {
+    type = bool
+    default = true
+}
+variable "block_public_access_policies" {
+    type = bool
+    default = true
+}
+variable "block_public_access_cross_accounts" {
+    type = bool
+    default = true
+}
+variable "bucket_versioning" {
+    type = bool
+    default = false
+}
+variable "force_destroy" {
+    type = bool
+    default = false
+}
+variable "website_index_document" {
+    type = set(string)
+    default = []
+}
+variable "website_cloudfront_origin_access_identity_arn" {
+    type = string
+    default = null
+}
+
+output "arn" {
+    value = aws_s3_bucket.s3_bucket.arn
+}
+output "bucket" {
+    value = var.bucket_name
+}
+output "bucket_domain_name" {
+    value = aws_s3_bucket.s3_bucket.bucket_domain_name
+}
+output "id" {
+    value = aws_s3_bucket.s3_bucket.id
+}
