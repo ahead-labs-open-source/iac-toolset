8800c10adbe1a33d65a5c714e0493aaae45762ca
Alberto Carbonell Rubio <alberto.carbonell@aheadlabs.com>
2021-06-09 13:34:44 +0200

    Added warm-up and tear-down actions using lambda invocation for existing pipelines.

--- aws/codepipeline/_variables.tf
+++ aws/codepipeline/_variables.tf
@@ -2,6 +2,10 @@ variable "aws_account_id" {type = string}
 variable "aws_region" {type = string}
 variable "codecommit_repository" {type = string}
 variable "deploy_to" {type = string}
+variable "sonarqube_starter_lambda_name" {
+    type = string
+    default = "ec2-handle-sonarqube-instance"
+    }
 variable "stage_build_config" {type = map}
 variable "stage_deploy_config_elasticbeanstalk" {type = map}
 variable "stage_deploy_config_s3" {type = map}
