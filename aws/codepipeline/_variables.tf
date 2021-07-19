996fb12b180b8300be1af6268dcee66ac8e97d1b
Iván Sainz <ivan.sainz@aheadlabs.com>
2021-07-19 17:53:32 +0200

    Added notification rules for CodePipeline pipelines on failures.

--- aws/codepipeline/_variables.tf
+++ aws/codepipeline/_variables.tf
@@ -2,10 +2,37 @@ variable "aws_account_id" {type = string}
 variable "aws_region" {type = string}
 variable "codecommit_repository" {type = string}
 variable "deploy_to" {type = string}
+variable "notification_event_type_ids" {
+    type = list
+    default = [
+        "codepipeline-pipeline-action-execution-succeeded",
+        "codepipeline-pipeline-action-execution-failed",
+        "codepipeline-pipeline-action-execution-canceled",
+        "codepipeline-pipeline-action-execution-started",
+        "codepipeline-pipeline-stage-execution-started",
+        "codepipeline-pipeline-stage-execution-succeeded",
+        "codepipeline-pipeline-stage-execution-resumed",
+        "codepipeline-pipeline-stage-execution-canceled",
+        "codepipeline-pipeline-stage-execution-failed",
+        "codepipeline-pipeline-pipeline-execution-failed",
+        "codepipeline-pipeline-pipeline-execution-canceled",
+        "codepipeline-pipeline-pipeline-execution-started",
+        "codepipeline-pipeline-pipeline-execution-resumed",
+        "codepipeline-pipeline-pipeline-execution-succeeded",
+        "codepipeline-pipeline-pipeline-execution-superseded",
+        "codepipeline-pipeline-manual-approval-failed",
+        "codepipeline-pipeline-manual-approval-needed",
+        "codepipeline-pipeline-manual-approval-succeeded"
+    ]
+}
+variable "notification_sns_topic_arn" {
+    type = string
+    default = null  
+}
 variable "sonarqube_starter_lambda_name" {
     type = string
     default = "ec2-handle-sonarqube-instance"
-    }
+}
 variable "stage_build_config" {type = map}
 variable "stage_deploy_config_elasticbeanstalk" {type = map}
 variable "stage_deploy_config_s3" {type = map}
