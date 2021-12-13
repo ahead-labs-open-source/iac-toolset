8d8ff75677bf2a7211f58c0855f1a228ad73de8c
Iván Sainz <ivan.sainz@aheadlabs.com>
2021-12-13 10:51:14 +0100

    Fixed last commit.

--- aws/route53/_variables.tf
+++ aws/route53/_variables.tf
@@ -2,20 +2,20 @@ variable "domain" {type = string}
 variable "zone_id" {type = string}
 
 variable "office_365_email" {
-    type = bool,
+    type = bool
     default = false
 }
 variable "office_365_email_msid" {
-    type = string,
+    type = string
     default = ""
 }
 
 variable "google_workspace_email" {
-    type = bool,
+    type = bool
     default = false
 }
 
 variable "google_workspace_site_verification" {
-    type = string,
+    type = string
     default = ""
 }
