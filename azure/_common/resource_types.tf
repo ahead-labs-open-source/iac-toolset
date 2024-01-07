98f3c945babf419e2e35d1079b7a6020f535cbf6
Iván Sainz <ivan.sainz@aheadlabs.com>
2024-01-07 02:28:22 +0100

    Added new resource types to Azure common module

--- azure/_common/resource_types.tf
+++ azure/_common/resource_types.tf
@@ -1,6 +1,14 @@
 variable "resource_types" {
   type = map(any)
   default = {
+    apim = {
+      name = "API Management"
+      tag  = "apim"
+    }
+    dns = {
+      name = "DNS"
+      tag  = "dns"
+    }
     rg = {
       name = "Resource group"
       tag  = "rg"
