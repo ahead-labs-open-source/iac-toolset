6a047a0b4bdc2f18d3e4dde86661d14f6b530c0f
Iván Sainz <ivan.sainz@aheadlabs.com>
2025-04-28 01:47:23 +0200

    chore(HIS-20): update common region locations (used in code)

--- azure/_common/locations.tf
+++ azure/_common/locations.tf
@@ -1,15 +1,26 @@
 variable "locations" {
   type = map(object({
     name = string
+    geography = string
     tags = map(string)
   }))
   default = {
     westeurope = {
       name = "West Europe"
+      geography = "Netherlands"
       tags = {
         long   = "westeurope"
         middle = "westeu"
         short  = "weu"
+      }
+    }
+    spaincentral = {
+      name = "Spain central"
+      geography = "Madrid, Spain"
+      tags = {
+        long   = "spaincentral"
+        middle = "spain"
+        short  = "es"
       }
     }
   }
